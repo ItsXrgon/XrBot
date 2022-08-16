9f297e33d3a97f0b1eec8eb3e03f305317693303
Ali <111145482+ItsXrgon@users.noreply.github.com>
2022-08-16 06:55:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from WebScrapingCommands import WebScrapingCommands
 from discord.ext import commands
 
 
-Bot = commands.Bot(command_prefix="x!", case_insensitive=True)
+Bot = commands.Bot(command_prefix="x!", case_insensitive=True)  # Adding prefix to bot
 
 @Bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(Bot))
-    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Learn Hebrew while sleeping 8h version"))
+    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Learn Hebrew while sleeping 8h version"))  # Bot status
     # Setting `Playing ` status
     #await client.change_presence(activity=discord.Game(name="pain"))
     
     # Setting `Watching ` status
     #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the errors"))
 
-Bot.remove_command('help')
+Bot.remove_command('help')  # Remove help command to add our own
 Bot.add_cog(MiniCommands(Bot))
 Bot.add_cog(MemoCommands(Bot))
 Bot.add_cog(WebScrapingCommands(Bot))
